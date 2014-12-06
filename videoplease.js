cd30b3cd81004e98e4ceb26e8a4a9b387c4475a6
planttheidea <tony.quetano@planttheidea.com>
2014-12-06 07:38:14 -0500

    Update videoplease.js

--- videoplease.js
+++ videoplease.js
@@ -1,3 +1,17 @@
+/*
+ *
+ * Copyright 2014 Tony Quetano under the terms of the MIT
+ * license found at https://github.com/planttheidea/bolster/MIT_License.txt
+ *
+ * videoplease.js - A plugin to generate bulletproof video that works back to IE8
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
 (function(window,document){
 		// create nodes with ease
 	var newNode = function(nodeName){
